9ebd99fccda6b44fefe891dcc040caea70112c01
Homa Wong <homawong@gmail.com>
2019-07-17 18:43:48 -0700

    fix: do not exist if test are suspended (#2)
    fix: typo
    feat: do not auto enable `bail`

--- src/ContinuePlugin.spec.ts
+++ src/ContinuePlugin.spec.ts
@@ -10,3 +10,23 @@ test('is not enabled on start', async () => {
   const subject = new ContinuePlugin({ config: {}, stdout: process.stdout })
   expect(subject.enabled).toBe(false)
 })
+
+test('enable will start tests', async () => {
+  const subject = new ContinuePlugin({ config: {}, stdout: process.stdout })
+  expect(await subject.run()).toBe(true)
+})
+
+test('when entering continue mode with no test, assume it is suspended and will not exist continue mode immediately', async () => {
+  const subject = new ContinuePlugin({ config: {}, stdout: process.stdout })
+  let completeHandler: any
+  subject.apply({
+    onFileChange() { },
+    shouldRunTestSuite() { },
+    onTestRunComplete(handler) { completeHandler = handler }
+  })
+
+  await subject.run()
+
+  completeHandler({ numTotalTestSuites: 0, testResults: [] })
+  expect(subject.enabled).toBe(true)
+})
